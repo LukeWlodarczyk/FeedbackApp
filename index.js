b4b6ad1ae6c9b1ff5a0b768b47a4279211f5d0e9
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-08 23:49:29 +0200

    Init commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-require('./services/passport');
 
 const app = express();
-
-app.use('/auth', require('./routes/auth'));
 
 const port = process.env.PORT || 5001;
 

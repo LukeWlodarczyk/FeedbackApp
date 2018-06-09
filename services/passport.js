ffd5688096eb8978112058ea2e28dfaaf1ede9ce
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-10 00:24:48 +0200

    correct typo

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
 	new GoogleStrategy(
 		{
 			clientID: keys.googleClientID,
-			clientSecret: keys.googleCLientSecret,
+			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback',
 			proxy: true,
 		},

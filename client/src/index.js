595a5ee77b33ad6576ef137ef93377eed1f0f766
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-10 23:31:30 +0200

    redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+const store = createStore(rootReducer, {}, applyMiddleware());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);

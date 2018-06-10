5e19bb23fa79c8e12279c30109bbe246c706d525
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-10 12:34:16 +0200

    proxy

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Helo you</h1>
+					<h1 className="App-title">Welcome to React</h1>
+					<a href="/auth/google">Login With Google</a>
 				</header>
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.

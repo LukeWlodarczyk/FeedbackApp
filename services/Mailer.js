a504d8eadae4efb02d31f2bc2e65470ad436bf44
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-14 01:16:10 +0200

    Mailer update

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,16 +1,16 @@
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
-
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
 	constructor({ subject, recipients }, content) {
 		super();
+
 		this.sgApi = sendgrid(keys.sendGridKey);
-		this.from_email = new helper.Email('@no-reply@feedbacky.com');
-		this.subjcet = subject;
+		this.from_email = new helper.Email('no-reply@emaily.com');
+		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
-		this.recipients = this.fromatAddresses(recipients);
+		this.recipients = this.formatAddresses(recipients);
 
 		this.addContent(this.body);
 		this.addClickTracking();

49b300bfa73ef51bc6888385a08c003a2d1ba72a
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-14 00:15:07 +0200

    Mailer update

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,16 +6,47 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
 	constructor({ subject, recipients }, content) {
 		super();
+		this.sgApi = sendgrid(keys.sendGridKey);
 		this.from_email = new helper.Email('@no-reply@feedbacky.com');
 		this.subjcet = subject;
 		this.body = new helper.Content('text/html', content);
-		this.recipients = this.frometAddresses(recipients);
+		this.recipients = this.fromatAddresses(recipients);
 
 		this.addContent(this.body);
+		this.addClickTracking();
+		this.addRecipients();
 	}
 
 	formatAddresses(recipients) {
 		return recipients.map(({ email }) => new helper.Email(email));
+	}
+
+	addClickTracking() {
+		const trackingSettings = new helper.TrackingSettings();
+		const clickTracking = new helper.ClickTracking(true, true);
+
+		trackingSettings.setClickTracking(clickTracking);
+		this.addTrackingSettings(trackingSettings);
+	}
+
+	addRecipients() {
+		const personalize = new helper.Personalization();
+
+		this.recipients.forEach(recipient => {
+			personalize.addTo(recipient);
+		});
+		this.addPersonalization(personalize);
+	}
+
+	async send() {
+		const request = this.sgApi.emptyRequest({
+			method: 'POST',
+			path: '/v3/mail/send',
+			body: this.toJSON(),
+		});
+
+		const response = await this.sgApi.API(request);
+		return response;
 	}
 }
 

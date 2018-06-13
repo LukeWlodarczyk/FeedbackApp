4f620c4664be8b64e9c8e08eb4e4dba8f0c6c510
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-14 01:17:14 +0200

    catch errors - survey route

--- routes/survey.js
+++ routes/survey.js
@@ -12,18 +12,23 @@ const surveyTemplate = require('../services/emailTemplates/survey');
 const Survey = mongoose.model('surveys');
 
 router.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-	const { title, subject, body, recipients } = req.body;
+	try {
+		const { title, subject, body, recipients } = req.body;
 
-	const survey = new Survey({
-		title,
-		subject,
-		body,
-		recipients: recipients.split(',').map(email => ({ email: email.trim() })),
-		_user: req.user.id,
-		dateSent: Date.now(),
-	});
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+			_user: req.user.id,
+			dateSent: Date.now(),
+		});
 
-	const mailer = new Mailer(survey, surveyTemplate(survey));
+		const mailer = new Mailer(survey, surveyTemplate(survey));
+		mailer.send();
+	} catch (e) {
+		res.status(400).send({ error: e });
+	}
 });
 
 module.exports = router;

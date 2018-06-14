a417e690f9a197ebe15094ff145853ea6fb8f6ab
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-14 02:52:02 +0200

    create survey and take credit

--- routes/survey.js
+++ routes/survey.js
@@ -12,22 +12,28 @@ const surveyTemplate = require('../services/emailTemplates/survey');
 const Survey = mongoose.model('surveys');
 
 router.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+	const { title, subject, body, recipients } = req.body;
+
+	const survey = new Survey({
+		title,
+		subject,
+		body,
+		recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+		_user: req.user.id,
+		dateSent: Date.now(),
+	});
+
+	const mailer = new Mailer(survey, surveyTemplate(survey));
+
 	try {
-		const { title, subject, body, recipients } = req.body;
+		await mailer.send();
+		await survey.save();
+		req.user.credits -= 1;
+		const user = await req.user.save();
 
-		const survey = new Survey({
-			title,
-			subject,
-			body,
-			recipients: recipients.split(',').map(email => ({ email: email.trim() })),
-			_user: req.user.id,
-			dateSent: Date.now(),
-		});
-
-		const mailer = new Mailer(survey, surveyTemplate(survey));
-		mailer.send();
-	} catch (e) {
-		res.status(400).send({ error: e });
+		res.send(user);
+	} catch (err) {
+		res.status(422).send(err);
 	}
 });
 

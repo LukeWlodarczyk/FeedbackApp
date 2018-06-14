c5ebf105151da15f83138ef6c6c8fb7b8171f7e0
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-14 02:49:04 +0200

    Mailer email update

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,7 +7,7 @@ class Mailer extends helper.Mail {
 		super();
 
 		this.sgApi = sendgrid(keys.sendGridKey);
-		this.from_email = new helper.Email('no-reply@emaily.com');
+		this.from_email = new helper.Email('no-reply@feedbacky.com');
 		this.subject = subject;
 		this.body = new helper.Content('text/html', content);
 		this.recipients = this.formatAddresses(recipients);

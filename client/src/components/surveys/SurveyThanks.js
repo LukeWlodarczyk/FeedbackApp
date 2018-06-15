64ff215b14fae7a7a09cc88a03b5f7a9a00c623f
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-15 23:25:07 +0200

    SurveyThanks

---
+++ client/src/components/surveys/SurveyThanks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SurveyThanks = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>Feedbacky</h1>
+			<p>Thanks for your Feedback!</p>
+		</div>
+	);
+};
+
+export default SurveyThanks;

9b8075a3668a0da78c4e00a9cf940a1417ad9a56
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-27 23:56:40 +0200

    survey list

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
 	return (
 		<div>
+			<SurveyList />
 			<div className="fixed-action-btn">
 				<Link to="/surveys/new" className="btn-floating btn-large pulse">
 					<i className="material-icons">add</i>
---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+	componentDidMount() {
+		this.props.fetchSurveys();
+	}
+
+	renderSurveys() {
+		return this.props.surveys.reverse().map(survey => {
+			return (
+				<div className="card darken-1" key={survey._id}>
+					<div className="card-content">
+						<span className="card-title">{survey.title}</span>
+						<p>{survey.body}</p>
+						<p className="right">
+							Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+						</p>
+					</div>
+					<div className="card-action">
+						<a>Yes: {survey.yes}</a>
+						<a>No: {survey.no}</a>
+					</div>
+				</div>
+			);
+		});
+	}
+
+	render() {
+		return <div>{this.renderSurveys()}</div>;
+	}
+}
+
+const mapStateToProps = ({ surveys }) => ({ surveys });
+
+export default connect(mapStateToProps, { fetchSurveys })(SurveyList);

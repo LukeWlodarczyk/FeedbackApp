476e2588fa7e639b247479ff20bc24420d40d182
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-06-27 23:57:18 +0200

    surveys reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import auth from './auth';
+import surveys from './surveys';
 
 export default combineReducers({
 	auth,
+	surveys,
 	form,
 });
